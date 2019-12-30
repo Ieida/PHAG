c08d150742481fcf9f67bdb3f7d0659a1803bb98
Ieida <37672966+Ieida@users.noreply.github.com>
2019-12-30 15:55:54 +0100

    Carry System
    The main carry system which let's the player pick up objects and drop them.

---
+++ Assets/Scripts/_custom/Mechanics/Carry/Data/CarryData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CarryDataObject", menuName = "ControllerData/Carry/CarryDataObject")]
+public class CarryData : ScriptableObject
+{
+    public float maxCarryWeight;
+    public float maxPickupRange;
+    public float dropDistance;
+    public LayerMask pickupItemMask;
+}
